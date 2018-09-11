b128ee13402f00ce33a5a0396372ca7b0bef0ee3
Tyler Bisbee <Tyler.Bisbee@gmail.com>
2018-09-11 15:38:08 -0400

    working db insert with timestamp

--- models/Note.js
+++ models/Note.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const NoteSchema = new Schema({
-    title:String
+    title:String,
+    createdDate: {
+        type: Date,
+        default: Date.Now
+    },
 });
 
 
