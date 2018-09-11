3edddc89421541fb56140e36660e9468a0170b67
Tyler Bisbee <Tyler.Bisbee@gmail.com>
2018-09-11 16:10:05 -0400

    missing file

---
+++ models/Comment.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    title:{},
+    body:{},
+    user: {}   
+}, {timestamps: true});
+
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+
+module.exports = Comment;
+
+
+
